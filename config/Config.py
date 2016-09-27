814e262f123fe78e9e5dac0f876a6ce11766bebe
hahahu <im@huhaoyu.com>
2016-09-27 11:52:14 +0800

    config: add configs(basic info, default info, reservations, timings)

---
+++ config/Config.py
@@ -0,0 +1,121 @@
+from utils.Singleton import Singleton
+from plugins.JsonReader import JsonReader
+from entities.User import User
+from entities.Stadium import Stadium
+from entities.Timing import Timing
+from entities.TimeInterval import TimeInterval
+from entities.ReservationCandidate import ReservationCandidate
+import os
+
+
+class Config(Singleton):
+    DEFAULT_CONFIG_FILE_NAME = 'default.json'
+    ACCOUNTS_FILE_NAME = 'accounts.json'
+    BASIS_FILE_NAME = 'basis.json'
+    RESERVATIONS_FILE_NAME = 'reservations.json'
+    TIMINGS_FILE_NAME = 'timings.json'
+
+    def __init__(self):
+        super(Config, self).__init__()
+        self.default_cache = None
+        self.accounts_cache = None
+        self.stadiums_cache = None
+        self.timings_cache = None
+        self.tasks_cache = None
+        self.standard_sections_cache = None
+
+    def get_accounts(self):
+        """
+        :rtype: dict
+        """
+        if self.accounts_cache is not None:
+            return self.accounts_cache
+
+        users = Config.read_configs(Config.get_curr_abs_path(Config.ACCOUNTS_FILE_NAME))
+        self.accounts_cache = {}
+        if users is not None:
+            for name in users:
+                item = users[name]
+                user = User(item['id'], item['username'], item['password'], item['phone'], item['user_type'])
+                self.accounts_cache[name] = user
+        return self.accounts_cache
+
+    def get_default_account(self):
+        """
+        :rtype: User
+        """
+        if self.default_cache is None:
+            self.default_cache = Config.read_configs(Config.get_curr_abs_path(Config.DEFAULT_CONFIG_FILE_NAME))
+        default_user = self.default_cache['account']
+        return self.get_accounts()[default_user]
+
+    def get_stadiums(self):
+        """
+        :rtype: list
+        """
+        if self.stadiums_cache is not None:
+            return self.stadiums_cache
+
+        stadiums = Config.read_configs(Config.get_curr_abs_path(Config.BASIS_FILE_NAME))
+        self.stadiums_cache = []
+        for item in stadiums:
+            self.stadiums_cache.append(Stadium.from_json(item))
+        return self.stadiums_cache
+
+    def get_timings(self):
+        """
+        :rtype: Timing
+        """
+        if self.timings_cache is not None:
+            return self.timings_cache
+
+        timings = Config.read_configs(Config.get_curr_abs_path(Config.TIMINGS_FILE_NAME))
+        self.timings_cache = Timing.from_json(timings)
+        return self.timings_cache
+
+    def get_reservation_tasks(self):
+        """
+        :rtype: list
+        """
+        if self.tasks_cache is not None:
+            return self.tasks_cache
+
+        tasks = Config.read_configs(Config.get_curr_abs_path(Config.RESERVATIONS_FILE_NAME))
+        self.tasks_cache = []
+        for l in tasks:
+            task = []
+            for obj in l:
+                candidate = ReservationCandidate.from_json(obj, self.get_standard_time_section(obj['section']))
+                task.append(candidate)
+            self.tasks_cache.append(task)
+
+        return self.tasks_cache
+
+    def get_standard_time_section(self, section_name):
+        """
+        :rtype: TimeInterval
+        """
+        if self.standard_sections_cache is None:
+            config = Config.read_configs(Config.get_curr_abs_path(Config.DEFAULT_CONFIG_FILE_NAME))
+            self.standard_sections_cache = config['unit']['section']
+        section = self.standard_sections_cache[section_name]
+        return TimeInterval(section['start'], section['end'])
+
+    def get_mail_account(self):
+        if self.default_cache is None:
+            self.default_cache = Config.read_configs(Config.get_curr_abs_path(Config.DEFAULT_CONFIG_FILE_NAME))
+        return self.default_cache['mail_account']
+
+    def get_mail_receivers(self):
+        if self.default_cache is None:
+            self.default_cache = Config.read_configs(Config.get_curr_abs_path(Config.DEFAULT_CONFIG_FILE_NAME))
+        return self.default_cache['receivers']
+
+    @staticmethod
+    def read_configs(path):
+        reader = JsonReader()
+        return reader.read(path)
+
+    @staticmethod
+    def get_curr_abs_path(file_name):
+        return os.path.dirname(os.path.abspath(__file__)) + os.sep + file_name
---
+++ config/readme.md
@@ -0,0 +1,36 @@
+## `unit`
+* `time`: 时间单位为`second/minute/hour`；
+* `week`: 从周一至周日分别对应0-6；
+* `section`: 早上8:00-12:00，下午12:00-18:00，晚上18:00-22:00。
+
+## `candidates`
+* `candidates`为一个主列表；
+* 主列表每个元素均为一个子列表，称为任务组，任务组中各任务的时间互斥，当程序完成其中一个预订任务后就会标记该任务组已完成；
+* 主列表的任务组之间不互斥，可以约定多个场地；
+
+## 任务详细参数
+* `week`为周次对应的`index`;
+* `wish`为期望的起始和终止时间;
+* `length`为持续时长;
+* `section`为时间段（早上、下午、晚上）;
+* `fixed`为是否强制采用`wish`规定的起止时间。当`fixed`为`false`时，当没有找到符合`wish`的空余场地时，会在规定的`section`时间段内寻找符合`length`的空余场地。
+
+## accounts.js
+```
+{
+  "your_name": {
+    "id": "your_student_id",
+    "username": "your_username",
+    "password": "your_password",
+    "phone": "your_phone_number",
+    "user_type": "student"
+  },
+  "other_name": {
+    "id": "other_student_id",
+    "username": "other_username",
+    "password": "other_password",
+    "phone": "other_phone_number",
+    "user_type": "student"
+  }
+}
+```
